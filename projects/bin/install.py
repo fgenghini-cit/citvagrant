f7362f97e0123eb4a730fc6b9b71fa9670d15019
jribeiro <jribeiro@ciandt.com>
2016-08-18 09:30:06 -0300

    Added step to install.py to create the vhost

--- projects/bin/install.py
+++ projects/bin/install.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import os
+import shutil
 import yaml
 
 def main():
@@ -8,7 +10,7 @@ def main():
     Project Installation Steps
     """
 
-    for project, cfg in yaml.load(open("../projects.yml", 'r')).iteritems():
+    for project, cfg in yaml.load(open("/files/projects/projects.yml", 'r')).iteritems():
         repo_name = cfg['link']['repo']
         platform_path = "/files/projects/platforms/%s" % cfg['link']['platform']
         repo_docroot = "/files/projects/repos/%s/source/deploy" % repo_name
@@ -19,10 +21,27 @@ def main():
         if not os.path.islink(multisite_path):
             os.symlink(repo_docroot, multisite_path)
 
-        # Add project to sites.php
-        # TODO: It sites.php doesn't exist, create it.
-        # TODO: Check if repo exist on sites.php, if not add new entry.
+        # Configure Apache Vhost.
+        token_mapping = {
+            "[[DOCROOT]]": platform_path,
+            "[[SERVERNAME]]": "%s.localhost" % repo_name,
+        }
 
+        apache_vhost_skel = "/files/provisioning/skel/vhost.skel.conf"
+        vhost_content = replace_file_content(apache_vhost_skel, token_mapping)
+        apache_vhost_dest = "/etc/apache2/sites-enabled/%s.localhost" % repo_name
+        vhost = open(apache_vhost_dest, 'w+')
+        vhost.write(vhost_content)
+        vhost.close()
+
+def replace_file_content(file, dic):
+    """ Find and replace file content. """
+
+    source = open(file)
+    content = source.read()
+    for i, j in dic.items():
+        content = content.replace(i, j)
+    return content
 
 if __name__ == '__main__':
     main()
