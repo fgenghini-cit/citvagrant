449b852ed7fa8ffe0760d581cda9e26caf4a29e9
jribeiro <jribeiro@ciandt.com>
2016-08-18 13:00:13 -0300

    Updated install.py

--- projects/bin/install.py
+++ projects/bin/install.py
@@ -24,48 +24,73 @@ def main():
             logging.warning("%s dir not found. Check repo configuration on projects.yml." % repo_docroot)
             continue
 
-        multisite_path = "%s/sites/%s.localhost" % (platform_path, repo_name)
-        sites_php = "%s/sites/sites.php" % platform_path
-
         # Creates symbolic link from repo to platform.
-        if not os.path.islink(multisite_path):
-            os.symlink(repo_docroot, multisite_path)
+        create_site_symbolic_link(platform_path, repo_name)
 
-        # Creates the sites.php file if does not exists.
-        if not os.path.exists(sites_php):
-            sites_php_file = open(sites_php, 'w+')
-            sites_php_file.close()
+        # Creates sites.php.
+        create_sites_php(platform_path, repo_name)
 
-        # Append the multi-site repo variable if it does not exists.
-        sites_php_file = open(sites_php, 'ab+')
-        if repo_name not in sites_php_file.read():
-            sites_php_file.write("$sites['%s.localhost'] = '%s.localhost';\n" % (repo_name, repo_name))
-        sites_php_file.close()
-
-        mysql = cfg['drupal']['settings']['mysql']
-        mapping = {
-            '[[DATABASENAME]]': mysql['db'],
-            '[[USERNAME]]': mysql['user'],
-            '[[PASSWORD]]': mysql['passwd'],
-            '[[LOCALHOST]]': mysql['host'],
-        }
-        settings_php_content = replace_file_content('/files/provisioning/skel/default.settings.php', mapping)
-        settings_php_file = open(repo_docroot + '/settings.php', 'w+')
-        settings_php_file.write(settings_php_content)
-        settings_php_file.close()
+        # Creates settings.php.
+        settings = cfg['drupal']['settings']
+        create_settings_php(settings, repo_docroot)
 
         # Configure Apache Vhost.
-        token_mapping = {
-            "[[DOCROOT]]": platform_path,
-            "[[SERVERNAME]]": "%s.localhost" % repo_name,
-        }
+        configure_site_apache_vhost(platform_path, repo_name)
 
-        apache_vhost_skel = "/files/provisioning/skel/vhost.skel.conf"
-        vhost_content = replace_file_content(apache_vhost_skel, token_mapping)
-        apache_vhost_dest = "/etc/apache2/sites-enabled/%s.localhost" % repo_name
-        vhost = open(apache_vhost_dest, 'w+')
-        vhost.write(vhost_content)
-        vhost.close()
+
+def create_site_symbolic_link(platform_path, repo_name):
+    """ Creates symbolic link from repo to platform. """
+
+    multisite_path = "%s/sites/%s" % (platform_path, repo_name)
+    repo_docroot = "/files/projects/repos/%s/source/deploy" % repo_name
+
+    if not os.path.islink(multisite_path):
+        os.symlink(repo_docroot, multisite_path)
+
+def create_sites_php(platform_path, repo_name):
+    """ Creates and populates sites.php """
+    # Creates the sites.php file if does not exists.
+    sites_php = "%s/sites/sites.php" % platform_path
+    if not os.path.exists(sites_php):
+        sites_php_file = open(sites_php, 'w+')
+        sites_php_file.close()
+
+    # Append the multi-site repo variable if it does not exists.
+    sites_php_file = open(sites_php, 'ab+')
+    if repo_name not in sites_php_file.read():
+        sites_php_file.write("$sites['%s.localhost'] = '%s.localhost';\n" % (repo_name, repo_name))
+    sites_php_file.close()
+
+def create_settings_php(settings, repo_docroot):
+    """ Creates and populates settings.php """
+
+    # Add MySQL settings.
+    mysql = settings['mysql']
+    mapping = {
+        '[[DATABASENAME]]': mysql['db'],
+        '[[USERNAME]]': mysql['user'],
+        '[[PASSWORD]]': mysql['passwd'],
+        '[[LOCALHOST]]': mysql['host'],
+    }
+    settings_php_content = replace_file_content('/files/provisioning/skel/default.settings.php', mapping)
+    settings_php_file = open(repo_docroot + '/settings.php', 'w+')
+    settings_php_file.write(settings_php_content)
+    settings_php_file.close()
+
+def configure_site_apache_vhost(platform_path, repo_name):
+    """ Creates and populates Apache Vhost """
+
+    token_mapping = {
+        "[[DOCROOT]]": platform_path,
+        "[[SERVERNAME]]": "%s.localhost" % repo_name,
+    }
+
+    apache_vhost_skel = "/files/provisioning/skel/vhost.skel.conf"
+    vhost_content = replace_file_content(apache_vhost_skel, token_mapping)
+    apache_vhost_dest = "/etc/apache2/sites-enabled/%s.localhost" % repo_name
+    vhost = open(apache_vhost_dest, 'w+')
+    vhost.write(vhost_content)
+    vhost.close()
 
 def replace_file_content(file, dic):
     """ Find and replace file content. """
